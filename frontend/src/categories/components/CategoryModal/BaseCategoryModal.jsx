17327b80b24f81469a236f05a1f9a557bf6a6d95
MateDawid <mateusiakdawid@gmail.com>
2026-01-19 10:37:18 +0100

    predictions_update: Do not allow to change Category deposit

--- frontend/src/categories/components/CategoryModal/BaseCategoryModal.jsx
+++ frontend/src/categories/components/CategoryModal/BaseCategoryModal.jsx
@@ -38,6 +38,7 @@ export default function BaseCategoryModal({
       label: 'Deposit',
       required: true,
       options: depositOptions,
+      disabled: editedCategory,
     },
     category_type: {
       type: 'select',
