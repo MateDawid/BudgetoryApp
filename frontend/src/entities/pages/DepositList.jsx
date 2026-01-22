6aa5b181837668219231a64fbf6b6dee30bb6e7d
MateDawid <mateusiakdawid@gmail.com>
2026-01-22 08:29:46 +0100

    info_boxes: Entity and Deposit InfoBoxes

--- frontend/src/entities/pages/DepositList.jsx
+++ frontend/src/entities/pages/DepositList.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import { Paper } from '@mui/material';
+import { Paper, Stack } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import EntityDataGrid, { EntityTypes } from '../components/EntityDataGrid';
+import InfoDialog from '../../app_infrastructure/components/InfoDialog';
+
+const INFO_DIALOG_DATA = {
+  title: 'What is Deposit?',
+  content: (
+    <Typography>
+      A Deposit represents an account or place where your funds are stored, such
+      as a bank account, brokerage account, cash wallet, or physical assets like
+      gold.
+    </Typography>
+  ),
+};
 
 /**
  * DepositList component to display list of Wallet Deposits.
  */
 export default function DepositList() {
   document.title = 'Deposits';
+  const [infoDialogOpened, setInfoDialogOpened] = useState(false);
 
   return (
     <Paper
@@ -18,12 +31,23 @@ export default function DepositList() {
         bgColor: '#F1F1F1',
       }}
     >
-      <Typography
-        variant="h4"
-        sx={{ display: 'block', color: '#BD0000', mb: 1 }}
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="flex-start"
+        spacing={1}
+        mb={1}
       >
-        Deposits
-      </Typography>
+        <Typography variant="h4" sx={{ display: 'block', color: '#BD0000' }}>
+          Deposits
+        </Typography>
+        <InfoDialog
+          dialogOpened={infoDialogOpened}
+          setDialogOpened={setInfoDialogOpened}
+          dialogTitle={INFO_DIALOG_DATA.title}
+          dialogContent={INFO_DIALOG_DATA.content}
+        />
+      </Stack>
       <Divider sx={{ mb: 1 }} />
       <EntityDataGrid entityType={EntityTypes.DEPOSIT} />
     </Paper>
