16915d0fd443032b0cb11666bf99f6750ace0717
MateDawid <mateusiakdawid@gmail.com>
2026-03-01 08:04:11 +0100

    style-polishing: Swagger styling for CurrencyViewSet

--- backend/src/wallets/views/currency_viewset.py
+++ backend/src/wallets/views/currency_viewset.py
@@ -1,10 +1,20 @@
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from app_infrastructure.utils.swagger_tag import swagger_tag
 from wallets.models import Currency
 from wallets.serializers.currency_serializer import CurrencySerializer
 
 
+@swagger_tag(
+    tag="03. Wallets",
+    action_params={
+        "list": {
+            "operation_summary": "Currencies list",
+            "operation_description": "List of all Currencies.",
+        },
+    },
+)
 class CurrencyViewSet(ListModelMixin, GenericViewSet):
     """View for retrieving list Currencies."""
 
